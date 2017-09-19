28220d86a38dceb67de9c767dd53116c282d246f
duaraghav8 <duaraghav8@gmail.com>
2017-09-19 17:49:08 +0530

    add peer dep

--- index.js
+++ index.js
@@ -6,31 +6,29 @@
 'use strict';
 
 module.exports = {
-
 	rules: {
 		'imports-on-top': 'error',
-                'variable-declarations': 'error',
-                'array-declarations': 'error',
-                'operator-whitespace': 'error',
-                'lbrace': 'error',
-                'mixedcase': 'warning',
-                'camelcase': 'warning',
-                'uppercase': 'warning',
-                'no-with': 'warning',
-                'no-empty-blocks': 'warning',
-                'no-unused-vars': 'error',
-                'blank-lines': 'warning',
-                'indentation': 'warning',
-                'overflow-indentation': 'warning',
-                'whitespace': 'warning',
-                'function-whitespace': 'error',
-                'semicolon-whitespace': 'error',
-                'comma-whitespace': 'error',
-                'conditionals-whitespace': 'error',
-                'operator-whitespace': 'error',
-                'deprecated-suicide': 'warning',
-                'pragma-on-top': 'warning',
-                'quotes': 'error'
+		'variable-declarations': 'error',
+		'array-declarations': 'error',
+		'operator-whitespace': 'error',
+		'lbrace': 'error',
+		'mixedcase': 'warning',
+		'camelcase': 'warning',
+		'uppercase': 'warning',
+		'no-with': 'warning',
+		'no-empty-blocks': 'warning',
+		'no-unused-vars': 'error',
+		'blank-lines': 'warning',
+		'indentation': 'warning',
+		'overflow-indentation': 'warning',
+		'whitespace': 'warning',
+		'function-whitespace': 'error',
+		'semicolon-whitespace': 'error',
+		'comma-whitespace': 'error',
+		'conditionals-whitespace': 'error',
+		'operator-whitespace': 'error',
+		'deprecated-suicide': 'warning',
+		'pragma-on-top': 'warning',
+		'quotes': 'error'
 	}
-
 };

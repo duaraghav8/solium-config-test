32694d2b4daea77a82e99b0ee228c35c47279645
duaraghav8 <duaraghav8@gmail.com>
2017-09-25 14:45:37 +0530

    docs correction

--- docs/build/html/searchindex.js
+++ docs/build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{chaati:0,index:0,modul:0,page:0,search:0},titles:["Welcome to TestCOnfig\u2019s documentation!"],titleterms:{chumma:0,document:0,indic:0,tabl:0,testconfig:0,welcom:0}})
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{chaati:[],index:0,modul:0,page:0,search:0},titles:["Welcome to TestCOnfig\u2019s documentation!"],titleterms:{chumma:[],document:0,indic:0,tabl:0,testconfig:0,welcom:0}})
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -18,7 +18,3 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-
-Chumma
-------
-- Chaati
